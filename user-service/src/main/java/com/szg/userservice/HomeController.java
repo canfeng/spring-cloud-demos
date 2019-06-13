a188724accb8cc8dcf54768e1e6ed77208c085a0
shizhiguo <shizhiguo@ibeesaas.com>
2019-06-13 20:02:03 +0800

    xx

--- user-service/src/main/java/com/szg/userservice/HomeController.java
+++ user-service/src/main/java/com/szg/userservice/HomeController.java
@@ -2,9 +2,12 @@ package com.szg.userservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * created by shizhiguo at 2019/6/11 21:18
@@ -14,6 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @Autowired
     private DiscoveryClient discoveryClient;
+    @Value("${info.city}")
+    String infoCity;
+
+    @GetMapping("/infoCity")
+    public String infoCity(){
+        return infoCity;
+    }
 
     @GetMapping("/user")
     public String dc() {
